a3201a34444e380d97fe28119ef5dbd8155070f3
mamali543 <amalyreda@gmail.com>
2024-08-13 20:44:22 +0100

    one shot

--- bonus/scripts/clear.sh
+++
@@ -1,9 +0,0 @@
-sudo gitlab-ctl stop
-sudo sv stop /opt/gitlab/service/*
-sudo pkill -f gitlab
-sudo rm -rf /opt/gitlab
-sudo rm -rf /var/opt/gitlab
-sudo rm -rf /var/log/gitlab
-sudo rm -rf /etc/gitlab
-#sudo reboot
-
